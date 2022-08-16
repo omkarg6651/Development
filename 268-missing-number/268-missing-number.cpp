6f988230a20ff60838764f52fdeb6e0c9470b0c7
omkarg6651 <91307686+omkarg6651@users.noreply.github.com>
2022-08-16 19:02:03 +0530

    Time: 43 ms (21.03%), Space: 17.9 MB (64.62%) - LeetHub

--- 268-missing-number/268-missing-number.cpp
+++ 268-missing-number/268-missing-number.cpp
@@ -24,3 +24,22 @@ public:
         
     }
 };
+
+/*
+for(i=0;i<=nums.size();i++)
+        {
+            for(j=0;j<nums.size();j++)
+            {
+                if(nums[j] == i)
+                {
+                    break;
+                }
+            }
+            if(j == nums.size())
+            {
+                break;
+            }
+        }
+        
+*/
+        
