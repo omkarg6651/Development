fd94b5893c6b8edcb203e2f38dfaf360f252faa6
omkarg6651 <91307686+omkarg6651@users.noreply.github.com>
2022-08-18 17:32:20 +0530

    Time: 15 ms (81.84%), Space: 5.8 MB (91.05%) - LeetHub

---
+++ 9-palindrome-number/9-palindrome-number.cpp
@@ -0,0 +1,34 @@
+class Solution {
+public:
+    bool isPalindrome(int x) {
+    
+        int iDigit = 0,iNo1 = 0;
+        long long int iNo2 = 0;
+        
+        if(x < 0)
+        {
+            return false;
+        }
+        
+        iNo1 = x;
+        
+        while(x != 0)
+        {
+            iDigit = x % 10;
+            iNo2 = (iNo2 *10) + iDigit;
+            x = x / 10;
+        }
+        
+        if(iNo1 == iNo2)
+        {
+            return true;
+        }
+        else
+        {
+            return false;
+        }
+        
+        
+        
+    }
+};
